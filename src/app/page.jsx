269951909ed7fd33eb5c3f32bdb9672eae72b937
szvitek <gy.szvitek@gmail.com>
2023-09-27 15:36:15 +0100

    feat(CSS): responsive layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,34 @@
-import Link from "next/link";
-import styles from "./homepage.module.css";
+import Link from 'next/link';
+import styles from './homepage.module.css';
 
 export default function Home() {
-  return <div><Link href="/">Hello</Link></div>;
+  return (
+    <div>
+      <Link href="/">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          ipsum aperiam placeat natus vitae pariatur veniam quae quis, mollitia
+          iste adipisci corporis recusandae delectus nihil eius ducimus tempore
+          magni quod!
+        </h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam minus
+        amet distinctio ducimus! Veritatis ducimus id nam adipisci commodi
+        consequuntur rem corrupti blanditiis quo numquam, earum ullam provident
+        voluptas officia? Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Aperiam minus amet distinctio ducimus! Veritatis ducimus id nam
+        adipisci commodi consequuntur rem corrupti blanditiis quo numquam, earum
+        ullam provident voluptas officia? Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Aperiam minus amet distinctio ducimus!
+        Veritatis ducimus id nam adipisci commodi consequuntur rem corrupti
+        blanditiis quo numquam, earum ullam provident voluptas officia? Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Aperiam minus amet
+        distinctio ducimus! Veritatis ducimus id nam adipisci commodi
+        consequuntur rem corrupti blanditiis quo numquam, earum ullam provident
+        voluptas officia? Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Aperiam minus amet distinctio ducimus! Veritatis ducimus id nam
+        adipisci commodi consequuntur rem corrupti blanditiis quo numquam, earum
+        ullam provident voluptas officia?
+      </Link>
+    </div>
+  );
 }
