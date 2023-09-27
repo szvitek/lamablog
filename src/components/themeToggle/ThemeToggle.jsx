6572b719fe2d203ceb9f40bdc1885565f7b8e0ac
szvitek <gy.szvitek@gmail.com>
2023-09-27 17:14:24 +0100

    feat(Context): toggle css mode dark/light functionality with context providers

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,11 +1,32 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './themeToggle.module.css';
+import { ThemeContext } from '@/context/ThemeContext';
+import { useContext } from 'react';
 
 const ThemeToggle = () => {
+  const { theme, toggle } = useContext(ThemeContext);
+
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === 'dark'
+          ? { backgroundColor: 'white' }
+          : { backgroundColor: '#0f172a' }
+      }
+    >
       <Image src="/moon.png" alt="" width={14} height={14} />
-      <div className={styles.toggle}></div>
+      <div
+        className={styles.toggle}
+        style={
+          theme === 'dark'
+            ? { left: 1, backgroundColor: '#0f172a' }
+            : { right: 1, backgroundColor: 'white' }
+        }
+      ></div>
       <Image src="/sun.png" alt="" width={14} height={14} />
     </div>
   );
