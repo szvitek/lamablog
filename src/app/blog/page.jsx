f0d2965e1844ea1c484efe87b3aa7e2ae9ea4000
szvitek <gy.szvitek@gmail.com>
2023-09-28 17:41:12 +0100

    feat(Blog): blog category page

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,16 @@
+import CardList from '@/components/cardList/CardList'
+import styles from './blog.module.css'
+import Menu from '@/components/menu/Menu'
+
+const BlogPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Style Blog</h1>
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  )
+}
+export default BlogPage
