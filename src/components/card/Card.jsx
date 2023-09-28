662c2505abe6a61635166ea416125a56323f50e1
szvitek <gy.szvitek@gmail.com>
2023-09-28 16:00:10 +0100

    feat(Card): blog card created

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import styles from './card.module.css';
+import Link from 'next/link';
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.02.2023 - </span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <Link href="/">
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+          similique, ea esse dignissimos ratione cum tenetur reprehenderit quae
+          nesciunt perspiciatis iste sequi optio cumque, saepe adipisci, veniam
+          ab ad eius!
+        </p>
+        <Link href="/" className={styles.link}>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Card;
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,11 +1,20 @@
+import Card from '../card/Card';
 import Pagination from '../pagination/Pagination';
 import styles from './cardList.module.css';
 
 const CardList = () => {
   return (
     <div className={styles.container}>
-      CardList
-      <Pagination /> 
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+      <Pagination />
     </div>
   );
 };
