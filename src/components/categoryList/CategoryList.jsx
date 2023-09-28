6dfd45de6cb33bc663c18e411d1d00e56e29e37a
szvitek <gy.szvitek@gmail.com>
2023-09-28 14:53:29 +0100

    feat(CategoryList): category component

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,6 +1,40 @@
+import Link from 'next/link';
 import styles from './categoryList.module.css';
+import Image from 'next/image';
+
+const categories = [
+  { name: 'style', bg: '#57c4ff31' },
+  { name: 'fashion', bg: '#da85c731' },
+  { name: 'food', bg: '#7fb88133' },
+  { name: 'travel', bg: '#ff795736' },
+  { name: 'culture', bg: '#ffb04f45' },
+  { name: 'coding', bg: '#5e4fff31' },
+];
 
 const CategoryList = () => {
-  return <div className={styles.container}>CategoryList</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {categories.map((category) => (
+          <Link
+            href={`/blog?cat=${category}`}
+            className={styles.category}
+            key={category.name}
+            style={{ backgroundColor: category.bg }}
+          >
+            <Image
+              src={`/${category.name}.png`}
+              alt=""
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+            {category.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default CategoryList;
