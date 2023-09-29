4eeeb12d0b1d9ecb3676e60c6e77e07733e3d085
szvitek <gy.szvitek@gmail.com>
2023-09-29 13:33:06 +0100

    feat(Write): add new post page design

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
   //temporary
-  const status = 'notauthenticated';
+  const status = 'authenticated';
   return (
     <>
       {status === 'notauthenticated' ? (
